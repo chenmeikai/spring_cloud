be6baa5c2e3476d17942c5d821de1952a3231b0c
chenmeikai <562899487@qq.com>
2018-10-11 17:56:02 +0800

    spring cloud demo

---
+++ spring_registry/src/main/java/com/kenhome/registry/SpringRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.kenhome.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRegistryApplication.class, args);
+    }
+}
